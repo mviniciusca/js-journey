cfd21bf27cfef8882b0f2156f4bb45132aec531f
Marcos Coelho <mviniciusca@gmail.com>
2025-04-20 11:15:59 -0300

    Refactor Variables.js to demonstrate variable declaration using var, const, and let; add explanatory comments on best practices.

--- Variables.js
+++ Variables.js
@@ -1,7 +1,25 @@
 // This is a JavaScript file that demonstrates variable declaration and usage
 
-var name = "John Doe"; 
+// var 
+var firstName = "John"; 
+var lastName = "Doe";
+var fullName = firstName + " " + lastName;
 var age = 34; 
 var isEmployed = true; 
 
-console.log("Name: " + name);
+console.log("Name: " + fullName); // deprecated we must use const or let
+
+
+// const
+const city = "New York";
+const country = "USA";
+const isStudent = false;
+console.log("City: " + city);
+
+
+// let 
+let score = 95;
+let grade = "A";
+console.log("Score: " + score);
+
+
