6ae17b88ecf47faa7aa22e98f86fae91cc5cc4b0
Marcos Coelho <mviniciusca@gmail.com>
2025-04-21 17:54:49 -0300

    Add Functions.js to define basic functions and demonstrate usage

---
+++ basics/Functions.js
@@ -0,0 +1,20 @@
+// Functions in Js
+
+
+function helloWorld() {
+    return "Hello World";
+}
+
+console.log(helloWorld()); // Hello World
+
+function sum (x, y) {
+    return x + y;
+}
+
+console.log(sum(5, 10)); // 15
+
+
+function divide (x, y) {
+    return x / y;
+}
+console.log(divide(10, 5)); // 2
