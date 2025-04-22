0b115ddf0c37477e1a76217cfc46d3a87b436f82
Marcos Coelho <mviniciusca@gmail.com>
2025-04-22 00:34:17 -0300

    Add initial implementation of person and color objects in JavaScript

--- basics/Objects.js
+++ basics/Objects.js
@@ -1,2 +1,21 @@
 // Objects in JavaScript
 
+const person = {
+    name: "John",
+    age: 30,
+    greet: function () {
+        console.log("Hello, my name is " + this.name);
+    }
+}
+
+const color = {
+    red: "#FF0000",
+    green: "#00FF00",
+    blue: "#0000FF",
+    getColor: function (colorName) {
+        return this[colorName];
+    }
+}
+
+// how to access object properties
+console.log(person.name); // John
